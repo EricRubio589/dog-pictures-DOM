9a2195302fa6ce07313e14ce6c06d434d031d808
Eric Rubio <eric.rubio.services@gmail.com>
2019-05-17 21:43:20 -0500

    Added DOM display

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function getImage() {
     fetch('https://dog.ceo/api/breeds/image/random')
      .then(response => response.json())
-     .then(responseJson => console.log(responseJson))
+     .then(responseJson => displayDogs(responseJson));
 }
 
 function handleButtonSubmit() {
@@ -12,6 +12,7 @@ function handleButtonSubmit() {
         let numberOfDogs = document.getElementById("numberInput").value
         logThePictures(numberOfDogs)
         console.log(numberOfDogs)
+        $('.picturesContainer').empty();
     });
 }
 
@@ -19,6 +20,13 @@ function logThePictures(number) {
     for (let i=0; i<number; i++){
         getImage();
     }
+}
+
+function displayDogs(responseJson) {
+    $('.picturesContainer').show()
+    $('.picturesContainer')
+    .append(`<img src="${responseJson.message}" class="results-img">`);
+    console.log(responseJson.message);
 }
 
 $(function initialize() {
